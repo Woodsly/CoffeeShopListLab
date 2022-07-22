76fa5c4a5868237b46119d8c143ecf133d3fa982
Connor <Woodconnor13@gmail.com>
2022-07-22 16:29:25 -0400

    Finished Coffee Shop Product List Lab

---
+++ CoffeeShopProducts/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using CoffeeShopProducts.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopProducts.Controllers
+{
+    public class ProductController : Controller
+    {
+        private CoffeeProductDBContext context = new CoffeeProductDBContext();
+        public IActionResult ProductDetails(int productId)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.Id == productId);
+            return View(p);
+        }
+        public IActionResult Products()
+        {
+            List<Product> products = context.Products.ToList();
+            return View(products);
+        }
+        public IActionResult Index()
+        {
+            return View(context.Products.ToList());
+        }
+    }
+}
